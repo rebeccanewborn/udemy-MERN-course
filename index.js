d3d31ff8017dd3567dff5dbe6bb42b4872d2f532
Rebecca Newborn <rjn58@cornell.edu>
2018-01-26 15:49:05 -0500

    google auth setup

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+require("./models/User");
+require("./services/passport");
 
+mongoose.connect(keys.mongoURI);
 const app = express();
+app.use(
+  cookieSession({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: [keys.cookieKey] })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
+
 // app.get("/greeting", (req, res) => {
 //   res.send({ hi: "there" });
 // });
-
-passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
